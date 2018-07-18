661d3bee9ca2e2ce6bcc6a8c3c2f12148e63c2d8
Sebastian Kim <sydneyitguy@gmail.com>
2018-07-18 22:38:02 +0900

    Refactoring, remove unecessary slash on routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
       patch 'refresh/@:author/:permlink', to: 'posts#refresh', constraints: { author: /[^\/]+/ }
       patch 'set_moderator/@:author/:permlink', to: 'posts#set_moderator', constraints: { author: /[^\/]+/ }
       patch 'moderate/@:author/:permlink', to: 'posts#moderate', constraints: { author: /[^\/]+/ }
-      get '/@:author', to: 'posts#author', constraints: { author: /([^\/]+?)(?=\.json|$|\/)/ } # override, otherwise it cannot include dots
-      get '/@:author/:permlink', to: 'posts#show', constraints: { author: /[^\/]+/ }
-      put '/@:author/:permlink', to: 'posts#update', constraints: { author: /[^\/]+/ }
-      delete '/@:author/:permlink', to: 'posts#destroy', constraints: { author: /[^\/]+/ }
-      get '/tag/:tag', to: 'posts#tag'      
+      get '@:author', to: 'posts#author', constraints: { author: /([^\/]+?)(?=\.json|$|\/)/ } # override, otherwise it cannot include dots
+      get '@:author/:permlink', to: 'posts#show', constraints: { author: /[^\/]+/ }
+      put '@:author/:permlink', to: 'posts#update', constraints: { author: /[^\/]+/ }
+      delete '@:author/:permlink', to: 'posts#destroy', constraints: { author: /[^\/]+/ }
+      get 'tag/:tag', to: 'posts#tag'      
     end
   end
 
