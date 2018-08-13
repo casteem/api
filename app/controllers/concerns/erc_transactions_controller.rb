408739a64a538e227e2a65d78f5ac9d05937c826
Sebastian Kim <sydneyitguy@gmail.com>
2018-08-13 18:20:55 +0900

    #48 - Rate limit withdraw

--- app/controllers/concerns/erc_transactions_controller.rb
+++ app/controllers/concerns/erc_transactions_controller.rb
@@ -14,7 +14,7 @@ class ErcTransactionsController < ApplicationController
     # if @erc_transaction.save
     #   render json: { success: true, balance: @current_user.reload.hunt_balance, withdrawal: @erc_transaction }
     # else
-    #   render json: { error: @erc_transaction.errors.full_messages.to_sentence }
+    #   render json: { error: @erc_transaction.errors.values.first }
     # end
   end
 end
--- app/models/erc_transaction.rb
+++ app/models/erc_transaction.rb
@@ -4,8 +4,9 @@ require 's_logger'
 class ErcTransaction < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :amount
-  validate :validate_hash_format, :check_balance
+  validate :validate_hash_format, :check_balance, :rate_limit
   scope :pending, -> { where(status: 'pending') }
+  scope :today, -> { where('created_at >= ?', 24.hours.ago) }
 
   VALID_STATUS = %w(pending running sent error)
   validates :status, inclusion: { in: VALID_STATUS }
@@ -19,6 +20,10 @@ class ErcTransaction < ApplicationRecord
 
   def check_balance
     errors.add(:amount, "Not enough balance") if user.hunt_balance < self.amount
+  end
+
+  def rate_limit
+    errors.add(:created_at, "You can only withdraw once a day") if ErcTransaction.where(user_id: self.user_id).today.exists?
   end
 
   def deduct_balance!
