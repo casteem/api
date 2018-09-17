e1a2e25a97b420a0773b9ae7fd6cde52216c7d09
Sebastian Kim <sydneyitguy@gmail.com>
2018-09-17 23:55:32 +0900

    Disable all bots

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,31 +4,31 @@ set :output, '/srv/web/steemhunt/shared/log/cron.log'
 ROOT_DIR = '/srv/web/steemhunt/current'
 RAKE_PATH = '/home/updatebot/.rbenv/shims/bundle exec rake'
 
-every :day, at: '12:01am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} mod_stats level_stats reward_voters reward_social_shares"
-end
+# every :day, at: '12:01am' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} mod_stats level_stats reward_voters reward_social_shares"
+# end
 
-every :day, at: '12:10am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] voting_bot2 && " +
-    "RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] && RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[8]"
-end
+# every :day, at: '12:10am' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] voting_bot2 && " +
+#     "RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] && RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[8]"
+# end
 
-every '0 4-23 * * *' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[0]"
-end
+# every '0 4-23 * * *' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[0]"
+# end
 
-every :day, at: '4:00am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} cleanup_hidden_posts"
-end
+# every :day, at: '4:00am' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} cleanup_hidden_posts"
+# end
 
-every :day, at: '5:00am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} daily_post"
-end
+# every :day, at: '5:00am' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} daily_post"
+# end
 
-every :day, at: '06:00am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} dump"
-end
+# every :day, at: '06:00am' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} dump"
+# end
 
-every :day, at: '06:00pm' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} detect_jerk"
-end
+# every :day, at: '06:00pm' do
+#   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} detect_jerk"
+# end
